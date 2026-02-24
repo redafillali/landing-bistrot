872b23a4dba4597971e2614e3fd756a2ebccc3ee
Reda EL Fillali <reda.elfillali@gmail.com>
2026-02-24 10:09:10 +0000

    Remove create and edit actions from reservation list and view pages, and update grillades image.

--- app/Filament/Resources/Reservations/Pages/ListReservations.php
+++ app/Filament/Resources/Reservations/Pages/ListReservations.php
@@ -12,8 +12,6 @@ class ListReservations extends ListRecords
 
     protected function getHeaderActions(): array
     {
-        return [
-            CreateAction::make(),
-        ];
+        return [];
     }
 }
